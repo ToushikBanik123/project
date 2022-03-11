e473a1b1cb638d406504101770ddf2bc4112e8e6
Toushik Banik <toushikbanikeducation@gmail.com>
2022-03-11 21:07:31 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/www/abc/com/project/MainActivity.kt
@@ -0,0 +1,6 @@
+package www.abc.com.project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp(home: Scaffold(
+    appBar: AppBar(
+      backgroundColor: Colors.red,
+      title: const Center(
+          child: Text('My 1st App')
+      ),
+    )
+  ),)
+  );
+}
